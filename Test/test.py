fe79f1d29cb237e6594ac123bf656ab546211689
Pelumi-Aderinto <aderintopelumi@gmail.com>
2022-09-26 09:25:39 +0100

    Cleaning up

--- Test/test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import requests
-
-df_allseasons = pd.read_csv('https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/cleaned_merged_seasons.csv', index_col = 'Unnamed: 0')
-df_allseasons.to_csv('all_seasons_hist.csv') 
